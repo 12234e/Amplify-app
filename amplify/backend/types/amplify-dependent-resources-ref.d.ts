539d2df8d562c2931daf53c450b78d4daf0ae360
1234e <mdalaminkhan19957@gmail.com>
2023-03-07 23:18:06 +0300

    changes for v2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp81021afb": {
       "AppClientID": "string",
